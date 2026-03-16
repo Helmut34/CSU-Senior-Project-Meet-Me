19b2dd0d6f62dd4b74bb6011ccd07a27df903325
Helmut Cespedes <helmut.cespedes@gmail.com>
2026-03-15 20:23:35 -0400

    Forgot to include env +working towards prod

--- backend/run.py
+++ backend/run.py
@@ -1,5 +1,9 @@
-from app import create_app
+from dotenv import load_dotenv
 import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '..', '.env'))
+
+from app import create_app
 
 app = create_app()
 
