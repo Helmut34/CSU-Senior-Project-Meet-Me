60448715c4574640b73da480f2207cf86fd7f3d9
Helmut Cespedes <helmut.cespedes@gmail.com>
2026-03-16 16:46:19 -0400

    Template Footer +Copyright +TemplateRef

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,26 @@
+export default function Footer() {
+	return (
+		<section id="footer">
+			<footer>
+				<ul className="icons">
+					<li>
+						<a href="#" className="icon brands alt fa-github">
+							<span className="label">Github</span>
+						</a>
+					</li>
+					<li>
+						<a href="#" className="icon solid alt fa-envelope">
+							<span className="label">Email</span>
+						</a>
+					</li>
+				</ul>
+				<ul className="copyright">
+					<li>&copy; Meet Me in the Middle</li>
+					<li>
+						Design: <a href="https://html5up.net">HTML5 UP</a>
+					</li>
+				</ul>
+			</footer>
+		</section>
+	)
+}
